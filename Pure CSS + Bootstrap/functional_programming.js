12616242fa6430122b443cdfb97a2d762d8e201a
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2025-04-01 11:05:52 +0100

    Add virtual pet object and console log message generators to functional_programming.js

--- Pure CSS + Bootstrap/functional_programming.js
+++ Pure CSS + Bootstrap/functional_programming.js
@@ -35,3 +35,84 @@ for (let i = 0; i < 10; i++){
     console.log( "*".repeat(i))
 }
 
+var virtualPet = {
+    sleepy: true,
+    nap: function() {
+        this.sleepy = false
+        return this.sleepy
+    }
+}
+
+console.log(virtualPet.sleepy)
+console.log(virtualPet.nap())
+console.log(virtualPet.sleepy)
+
+// Task 1: Build a function-based console log message generator
+function consoleStyler(color, background, fontSize, txt) {
+    // Declare a variable for the message
+    // WRITE YOUR CODE HERE
+    var message;
+    message = `%c${txt}`
+
+    // Declare a variable for the style
+    // WRITE YOUR CODE HERE
+    var style = `color: ${color}`
+    
+    // Append background style to the style variable
+    // WRITE YOUR CODE HERE
+    style += `background: ${background};`;
+    
+    // Append font size style to the style variable
+    // WRITE YOUR CODE HERE
+    style += `font-size ${fontSize};`;
+    
+    // Log the message with the applied style
+    // WRITE YOUR CODE HERE
+    console.log(message,style)
+}
+consoleStyler("blue", "yellow", "15px", "Meleyotan")
+
+// Task 2: Build another console log message generator
+function celebrateStyler(reason) {
+    // Declare a fontStyle variable with default styles
+    // WRITE YOUR CODE HERE
+    var fontStyle = "color: tomato; font-size: 50px";
+    // Check if the reason is "birthday"
+    // WRITE YOUR CODE HERE
+    if (reason === "birthday") {
+        console.log("%cHappy birthday", fontStyle)
+    }
+    // If reason is "champions", log a congrats message
+    // WRITE YOUR CODE HERE
+    else if (reason === "champions") {
+        console.log("%cCongrats on the title!", fontStyle)
+    }
+    else {
+        consoleStyler("blue", "yellow", "15px", "Meleyotan")
+    }
+    // For any other reason, log a default message
+    // WRITE YOUR CODE HERE
+}
+celebrateStyler("birthday")
+
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+// Call the consoleStyler function with appropriate arguments
+// WRITE YOUR CODE HERE
+
+// Call the celebrateStyler function with an appropriate argument
+// WRITE YOUR CODE HERE
+
+// Task 4: Insert a congratulatory and custom message
+function styleAndCelebrate(color, background, fontSize, txt, reason) {
+    // Call consoleStyler with the first four arguments
+    // WRITE YOUR CODE HERE
+
+    // Call celebrateStyler with the last argument
+    // WRITE YOUR CODE HERE
+
+}
+
+styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', 'champions');
+// Call styleAndCelebrate with appropriate arguments
+// WRITE YOUR CODE HERE
+

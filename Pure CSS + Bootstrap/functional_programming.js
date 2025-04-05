4d2a08ed4890edf356c8ff2117b6ba0be1c2c5ca
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2025-04-05 16:23:38 +0100

    Add Polygon class with perimeter calculation to functional_programming.js

--- Pure CSS + Bootstrap/functional_programming.js
+++ Pure CSS + Bootstrap/functional_programming.js
@@ -327,3 +327,24 @@ console.log(manager())
 
 
 
+class Polygon{
+    constructor(arr){
+        this.len_arr = arr
+    }
+    perimeter(){
+        let perimeter = 0
+        for (let i = 0; i < this.len_arr.length; i++){
+            perimeter = perimeter + this.len_arr[i]
+        }
+        return perimeter
+    }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
+
